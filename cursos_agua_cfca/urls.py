4dee7b942f62fd2821700d480330ae25d72c574f
Erick Murillo Molina <erickmurillo22@gmail.com>
2020-04-13 08:11:26 -0600

    template covid

--- cursos_agua_cfca/urls.py
+++ cursos_agua_cfca/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('aprendizaje.urls')),
-    path('covid19-nicaragua/', TemplateView.as_view(template_name="covid.html")),
+    path('nicaragua/covid19', TemplateView.as_view(template_name="covid.html")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
