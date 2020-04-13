b316190a0d2400242ae7361352a5bb5c0672d9af
Erick Murillo Molina <erickmurillo22@gmail.com>
2020-04-13 08:01:22 -0600

    covid

--- cursos_agua_cfca/urls.py
+++ cursos_agua_cfca/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('aprendizaje.urls')),
+    path('covid19-nicaragua/', TemplateView.as_view(template_name="covid.html")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
